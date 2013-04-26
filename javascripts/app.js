8cd67a53624c95975cb96fc8429411356842719e
simplelife7 <reeqi@ReeQitekiMacBook-Air.local>
2013-04-26 23:17:09 +0800

    test
    test

---
+++ javascripts/app.js
@@ -0,0 +1,33 @@
+$.getScript("http://127.0.01/root.js");
+
+// $.ajax({
+//    url:'http://127.0.01/option.js',
+//    dataType:"jsonp",
+//    jsonp:"jsonpcallback",
+//    success:function(data){
+//    //	alert(1);
+//        $('body').append('<script>'+data+'</script>');
+
+//        //var jsonData = data.replace('var xxInit = ');
+//    }
+// });
+
+
+// setTimeout(function(){
+// 	console.log($('#test'));
+// },1000);
+
+
+
+
+// //$("body").append('<div class="dddd"></div>');
+// $("body").append('<script type="text/javascript">function test(){console.log("e")}</script>');
+
+// $.ajax({
+//    url:'http://static.gtimg.com/icson/js/lab/option.js',
+//    dataType:"jsonp",
+//    success:function(data){
+//    	console.log(data);
+//       $("body").append('<script type="text/javascript">'+ data +'</script>');   	
+//    }
+// }); 
