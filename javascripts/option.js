bd7450ba1fdfa38038b43b9744d74456f1ebe9ed
simplelife7 <reeqi.wu@gmail.com>
2013-05-20 00:08:13 +0800

    修改option

--- javascripts/option.js
+++ javascripts/option.js
@@ -1,8 +1,8 @@
 var xDemo ={
 		"name" : "oxox用于测试的oxox",
-		"version" : "0.1",
-    "css" : "http://127.0.0.1/b.css",
-    "js" : "http://127.0.0.1/a.js",
+		"version" : "0.2",
+    "css" : ["http://127.0.0.1/b.css"],
+    "js" : ["http://127.0.0.1/a.js"],
     "matches" :"http://*.51buy.com/*"
 }
 
