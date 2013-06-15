c797db87cb53f5746cd9f161a9bb439d69ce7fa4
simplelife7 <reeqi.wu@gmail.com>
2013-06-16 02:32:20 +0800

    更新文件

---
+++ javascripts/mp_weixin_youhua.js
@@ -0,0 +1,10 @@
+{
+  "name": "微信公众帐号文章排版优化",
+  "version": "1.0",
+  "css": [""],
+  "js": ["http://userstyles.org/styles/userjs/86845/mp-weixin.user.js"],
+  "matches": "http://mp.weixin.qq.com/",
+  "ename": "mp_weixin_paiban",
+  "author": "reeqiwu",
+  "descrition": "微信公众帐号文章排版优化"
+}
--- javascripts/option.js
+++
@@ -1,8 +0,0 @@
-var xDemo ={
-		"name" : "oxox用于测试的一货oxox",
-		"version" : "0.1",
-    "css" : ["http://127.0.0.1/xdemo/b.css"],
-    "js" : ["http://127.0.0.1/xdemo/a.js"],
-    "matches" :"http://*.51buy.com/*"
-}
-
